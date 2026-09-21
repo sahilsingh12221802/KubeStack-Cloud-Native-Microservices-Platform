76f53d20bf465f42df414e9703aaae589f93bf21
Sahil Singh <hisahilsingh730@gmail.com>
2026-09-21 23:41:49 +0530

    Added Github Actions CI/CD Pipeline

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -33,3 +33,93 @@ resource "aws_iam_instance_profile" "kubestack_ec2" {
   name = "kubestack-ec2-profile"
   role = aws_iam_role.kubestack_ec2.name
 }
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  tags = {
+    Name        = "github-actions-oidc"
+    Project     = "KubeStack"
+    Environment = "development"
+  }
+}
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "kubestack-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:sahilsingh12221802/KubeStack-Cloud-Native-Microservices-Platform:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "kubestack-github-actions-role"
+    Project     = "KubeStack"
+    Environment = "development"
+  }
+}
+
+
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "kubestack-github-actions-ecr"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = [
+          aws_ecr_repository.backend.arn,
+          aws_ecr_repository.frontend.arn
+        ]
+      }
+    ]
+  })
+}
